c315eba77f979b69248f724f8e4757326c0df1a4
INSANE-GL1TCH <gautamishan81@gmail.com>
2025-05-25 13:45:15 +0545

    All done

--- src/controller/SignupController.java
+++ src/controller/SignupController.java
@@ -6,6 +6,10 @@ package controller;
 
 import Dao.UserDao;
 import View.SignUp;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import model.UserData;
 
 /**
  *
@@ -25,4 +29,27 @@ this.userView.setVisible(true);
 public void close() {
 this.userView.dispose();
 }
+
+    class AddUserListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                String name = userView.getUsernamefield().getText();
+                String email = userView.getEmailfield().getText();
+                String password = userView.getPasswordfield().getText();
+                UserData user = new UserData(name, email, password);
+                boolean check = userDao.checkUser(user);
+                if(check){
+                JOptionPane.showMessageDialog(userView, "Duplicate user");
+                }else{
+                userDao.signUp(user);
+                }
+            } catch (Exception ex) {
+            System.out.println("Error adding user: " + ex.getMessage());
+            }
+        }
+    }
 }
+    
+
