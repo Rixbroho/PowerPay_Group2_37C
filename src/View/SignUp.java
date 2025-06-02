b5500bdf0b83e6fff625ab7375f81eba5ebfdbee
Rixbroho <ratnaprajapati10@gmail.com>
2025-06-02 21:45:31 +0545

    to change branch

--- src/View/SignUp.java
+++ src/View/SignUp.java
@@ -181,6 +181,7 @@ public class SignUp extends javax.swing.JFrame {
         loginbtn.setBorder(null);
         loginbtn.setBorderPainted(false);
         loginbtn.setContentAreaFilled(false);
+        loginbtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         loginbtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 loginbtnActionPerformed(evt);
