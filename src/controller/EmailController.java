60ba7e287685e294f4448ff13a9d1ef133bac8a8
Rixbroho <ratnaprajapati10@gmail.com>
2025-06-04 20:38:51 +0545

    ok

--- src/controller/EmailController.java
+++ src/controller/EmailController.java
@@ -6,7 +6,7 @@ public class EmailController {
     public static boolean sendVerificationCode(String toEmail) {
         String subject = "Your Verification Code";
         String code = String.valueOf((int)(Math.random() * 9000) + 1000); // 4-digit code
-        String message = "Your code is: " + code;
+        String message = "Your code is " + code;
 
         return EmailSender.sendEmail(toEmail, subject, message);
     }
